fce22b81e155c52eb05a7f7ba9090ae644973152
Михаил <@LAPTOP-2364SFMN>
2022-10-31 15:17:05 +0300

    Добавьте файлы проекта.

---
+++ Lesson_bool/Lesson_bool/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lesson_bool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string A;
+            //string B;
+            //bool C = (A != B);
+
+            int A;
+            int B;
+            double X;
+            double Y;
+            bool C = (A < B) | (X > Y);
+        }
+    }
+}
